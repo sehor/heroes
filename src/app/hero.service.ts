73aeeab8bc884d5e9e144064578d0aa1a5a891cf
sehor <714534524@qq.com>
2019-04-18 17:19:00 +0800

    commit -m "service#1"

---
+++ src/app/hero.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HEROES} from './heroes-mock';
+import {Hero} from './hero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+
+   getHeroes():Hero[]{
+      return HEROES;
+   }
+}
---
+++ src/app/heroes-mock.ts
@@ -0,0 +1,6 @@
+import { Hero } from './hero';
+
+export const  HEROES :Hero[] =[
+  {id:11,name:"Super Man"},
+  {id:12,name:"Bat Man"},
+]
